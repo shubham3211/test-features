7723074412ca12012d4b15d928d288101e521c31
Ashish Kuamr <entropy.ash@gmail.com>
2017-10-08 14:59:16 +0530

    Automate callbacks in autotyping

--- AutoTyping/typingCode.js
+++ AutoTyping/typingCode.js
@@ -1,34 +1,52 @@
-let text = $("h1").text();
+let stringQueue;
 
-function typeText(jqObjectTextTag) {
-    let text = jqObjectTextTag.text();
-    jqObjectTextTag.text("");
-    let typingDelay = 0;
-    for (let i = 0; i < text.length; i++) {
-        typingDelay += generateRandomNumber(50, 500);
-        setTimeout(() => {
-            jqObjectTextTag.append(text.charAt(i));
-        }, typingDelay)
-    }
-    setTimeout(() => {
-        deleteText(jqObjectTextTag);
-    }, typingDelay + 2500)
+// Call funtion type(jqTextObject, str1, str2, str3...)
+// Last string stays on screen
+
+function type(...n) {
+    let jqTextObject = n.shift();
+    stringQueue = n;
+    typeAndDeleteNextString(jqTextObject);
 }
 
-function deleteText(jqObjectTextTag) {
-    let text = jqObjectTextTag.text();
+function typeAndDeleteNextString(jqTextObject) {
+    console.log("typeAndDeleteNextString()");
+    if (stringQueue.length != 0) {
+        typeString(jqTextObject, stringQueue.shift());
+    }
+}
+
+function typeString(jqTextObject, str) {
+    jqTextObject.text("");
+    let typingDelay = 0;
+    for (let i = 0; i < str.length; i++) {
+        typingDelay += generateRandomNumber(50, 500);
+        setTimeout(() => {
+            jqTextObject.append(str.charAt(i));
+        }, typingDelay)
+    }
+    if (stringQueue.length > 1) {
+        setTimeout(() => {
+            deleteText(jqTextObject);
+        }, typingDelay + 2500)
+    }
+}
+
+function deleteText(jqTextObject) {
+    let text = jqTextObject.text();
     let deleteDelay = 0;
     for (let i = text.length - 1; i >= 0; i--) {
         deleteDelay += 50;
         setTimeout(() => {
-            jqObjectTextTag.text(text.substring(0, i));
+            jqTextObject.html(text.substring(0, i));
         }, deleteDelay);
     }
-
+    deleteDelay += 200;
+    setTimeout(() => {typeAndDeleteNextString(jqTextObject)}, deleteDelay)
 }
 
 function generateRandomNumber(min,max) {
     return Math.floor(Math.random() *( max - min + 1) + min);
 }
 
-typeText($("h1"));
+type($("h1"), "This is a test string!", "This is a test string!", "This is a test string!", "This is a test string!");
